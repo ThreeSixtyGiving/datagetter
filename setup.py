e66952cc40c86edc4d3d0ca1cac1d9f8523aa182
Michael Wood <michael.wood@opendataservices.coop>
2019-08-09 17:11:31 +0100

    setup/requirements Make sure we install ourselves
    so we can run the datagetter.py outside the source directory

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python3
+
 from distutils.core import setup
+
+install_requires = []
+
+with open('./requirements.txt') as requirements_txt:
+    requirements = requirements_txt.read().strip().splitlines()
+    for requirement in requirements:
+        if requirement.startswith('#'):
+            continue
+        elif requirement.startswith('-e '):
+            install_requires.append(requirement.split('=')[1])
+        else:
+            install_requires.append(requirement)
 
 setup(
     name='datagetter',
@@ -9,4 +23,7 @@ setup(
     url='https://github.com/ThreeSixtyGiving/datagetter',
     license='LICENSE',
     description='Fetches the data from data.threesixtygiving.org',
+    install_requires=install_requires,
+    packages=['getter'],
+    packages_dir={'getter': 'getter'}
 )
