9222d58837c481be4a7b8b96fec20cc59f3ec924
Michael Wood <michael.wood@opendataservices.coop>
2025-02-17 09:26:04 +0000

    lint: Run python-black

--- datagetter.py
+++ datagetter.py
@@ -6,36 +6,72 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--no-download', dest='download', action='store_false',
-            help="Don't download any files only convert existing data")
+    parser.add_argument(
+        "--no-download",
+        dest="download",
+        action="store_false",
+        help="Don't download any files only convert existing data",
+    )
 
-    parser.add_argument('--local-registry', dest='local_registry', action='store',
-            help="Use a local registry file rather than registry.threesixtygiving.org",
-            default=False)
+    parser.add_argument(
+        "--local-registry",
+        dest="local_registry",
+        action="store",
+        help="Use a local registry file rather than registry.threesixtygiving.org",
+        default=False,
+    )
 
-    parser.add_argument('--no-convert', dest='convert', action='store_false')
-    parser.add_argument('--no-convert-big-files', dest='convert_big_files', action='store_false')
-    parser.add_argument('--no-validate', dest='validate', action='store_false')
-    parser.add_argument('--data-dir', dest='data_dir', action='store',
-                        default="data")
-    parser.add_argument('--threads', dest='threads', action='store', type=int,
-                        default=4)
-    parser.add_argument('--socks5', dest='socks5_proxy', action='store',
-                        help="Use a socks5 proxy to fetch publisher data. Example --socks5=socks5://host:port",
-                        default=None)
+    parser.add_argument("--no-convert", dest="convert", action="store_false")
+    parser.add_argument(
+        "--no-convert-big-files", dest="convert_big_files", action="store_false"
+    )
+    parser.add_argument("--no-validate", dest="validate", action="store_false")
+    parser.add_argument("--data-dir", dest="data_dir", action="store", default="data")
+    parser.add_argument(
+        "--threads", dest="threads", action="store", type=int, default=4
+    )
+    parser.add_argument(
+        "--socks5",
+        dest="socks5_proxy",
+        action="store",
+        help="Use a socks5 proxy to fetch publisher data. Example --socks5=socks5://host:port",
+        default=None,
+    )
 
-    parser.add_argument('--limit-downloads', dest='limit_downloads', action='store',
-                        type=int,
-                        help="Limit the number of file downloads",
-                        default=None)
+    parser.add_argument(
+        "--limit-downloads",
+        dest="limit_downloads",
+        action="store",
+        type=int,
+        help="Limit the number of file downloads",
+        default=None,
+    )
 
-    parser.add_argument('--schema-branch', dest='schema_branch', action='store',
-                        type=str,
-                        help="Specify a git branch of the 360Giving schema",
-                        default='master')
+    parser.add_argument(
+        "--schema-branch",
+        dest="schema_branch",
+        action="store",
+        type=str,
+        help="Specify a git branch of the 360Giving schema",
+        default="master",
+    )
 
-    parser.add_argument("--publishers", nargs="+", dest="publisher_prefixes", action="store",
-                        type=str, help="Only download for selected publishers")
+    parser.add_argument(
+        "--publishers",
+        nargs="+",
+        dest="publisher_prefixes",
+        action="store",
+        type=str,
+        help="Only download for selected publishers",
+    )
+
+    parser.add_argument(
+        "--file",
+        dest="test_file",
+        action="store",
+        type=str,
+        help="Test a particular file",
+    )
 
     args = parser.parse_args()
 
--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@ from setuptools import setup
 
 install_requires = []
 
-with open('./requirements.txt') as requirements_txt:
+with open("./requirements.txt") as requirements_txt:
     requirements = requirements_txt.read().strip().splitlines()
     for requirement in requirements:
-        if requirement.startswith('#'):
+        if requirement.startswith("#"):
             continue
-        elif requirement.startswith('-e '):
-            install_requires.append(requirement.split('=')[1])
+        elif requirement.startswith("-e "):
+            install_requires.append(requirement.split("=")[1])
         else:
             install_requires.append(requirement)
 
 setup(
-    name='datagetter',
-    version='0.1.0',
-    author='Open Data Services',
-    author_email='contact@opendataservices.coop',
-    scripts=['datagetter.py'],
-    url='https://github.com/ThreeSixtyGiving/datagetter',
-    license='LICENSE',
-    description='Fetches the data from registry.threesixtygiving.org',
+    name="datagetter",
+    version="0.1.0",
+    author="Open Data Services",
+    author_email="contact@opendataservices.coop",
+    scripts=["datagetter.py"],
+    url="https://github.com/ThreeSixtyGiving/datagetter",
+    license="LICENSE",
+    description="Fetches the data from registry.threesixtygiving.org",
     install_requires=install_requires,
-    packages=['getter'],
-    packages_dir={'getter': 'getter'}
+    packages=["getter"],
+    packages_dir={"getter": "getter"},
 )
