acef9fa765f7e879bdbdc44c9bcb4653534b9059
Michael Wood <michael.wood@opendataservices.coop>
2025-02-27 16:00:32 +0000

    tests:test: Improve comments/doc strings

--- tests/test.py
+++ tests/test.py
@@ -20,6 +20,13 @@ TEST_SERVER_PORT = 8888  # this is what the registry.json urls are expecting
 
 
 class ReusableTCPServer(socketserver.TCPServer):
+    """
+    Sets allow_reuse_address to true from default of false. This is so that if
+    the tests are rapidly run in succession we don't need to wait for the OS to
+    fully close any open sockets (and thus resulting in an
+    "Address already in use" error.
+    """
+
     allow_reuse_address = True
 
 
@@ -89,18 +96,24 @@ def test_expected_output(test_server):
     )
 
     def remove_known_differences(item):
+        """
+        Removes timestamp and full path which will be different
+        depending on when/where this test is being run
+        """
         del item["datagetter_metadata"]["datetime_downloaded"]
         if json_path := item["datagetter_metadata"].get("json"):
             item["datagetter_metadata"]["json"] = os.path.basename(json_path)
 
         return item
 
-    # Remove timestamps so these don't cause comparison failure
+    # Remove known differences
     expected_data_all = [remove_known_differences(item) for item in expected_data_all]
     fetched_data_all = [remove_known_differences(item) for item in fetched_data_all]
 
+    # Check data_all's match
     assert fetched_data_all == expected_data_all
 
+    # Compare other files produced with the expected files
     for file in ["aninvalidfile.json", "conversionerrorsfile.json", "validfile.json"]:
         expected = json.load(open(os.path.join(expected_data_dir, "json_all", file)))
         fetched = json.load(open(os.path.join(fetched_output_dir, "json_all", file)))
