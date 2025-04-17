073cc9c4f4c06f874bb1b5bfadf2a186cf28da10
Michael Wood <michael.wood@opendataservices.coop>
2025-04-17 15:51:52 +0100

    get: Add a developer option to enable HTTP request debugging

--- getter/get.py
+++ getter/get.py
@@ -18,6 +18,26 @@ from lib360dataquality.cove.schema import Schema360
 from lib360dataquality.cove.threesixtygiving import common_checks_360
 import getter.cache as cache
 
+
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+HTTP_DEBUG = False
+
+if HTTP_DEBUG:
+    import http.client as http_client
+    import logging
+
+    http_client.HTTPConnection.debuglevel = 1
+
+    # You must initialize logging, otherwise you'll not see debug output.
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
 acceptable_licenses = [
     "http://www.opendefinition.org/licenses/odc-pddl",
     "https://creativecommons.org/publicdomain/zero/1.0/",
